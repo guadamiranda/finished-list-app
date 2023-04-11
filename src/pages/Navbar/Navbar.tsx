1330191f0a138316a4dc76ae5de8de7b1494bbd8
Guada <guadamiranda@justiciacordoba.gob.ar>
2023-04-11 13:38:35 -0300

    Navbar and background

--- src/pages/Navbar/Navbar.tsx
+++ src/pages/Navbar/Navbar.tsx
@@ -1,9 +1,13 @@
-import './navbar.scss'
+import navbarStyle from './navbar.module.scss'
+import { MdOutlineVideogameAsset } from 'react-icons/md'
 
 export default function Navbar() {
     return(
-        <div className='navbar'>
-            Hi!
+        <div className={navbarStyle.navbar}>
+            <div className={navbarStyle.navbar__left}><div className={navbarStyle.navbar__hide}>My</div> <MdOutlineVideogameAsset className={navbarStyle.navbar__iconLogo}/> <div className={navbarStyle.navbar__hide}>List</div></div>
+            <div className={navbarStyle.navbar__center}>
+            </div>
+            <div className={navbarStyle.navbar__right}>tal</div>
         </div>
     )
 }
